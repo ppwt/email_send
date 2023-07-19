2d2c62e0bee9d2bb242e3f1875df40dcf0d7e5d1
ppwt <saraswatik@audaxlabs.com>
2023-07-19 17:48:24 +0530

    new mailer view created

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,2 +1,12 @@
 class UserMailer < ApplicationMailer
+    default from: 'notifications@example.com'
+
+  def welcome_email(user)
+    @user = user
+    mail(to: @user.email, subject: "Welcome to My Awesome Site looged in as #{@user.email}")
+  end
+
+  def good_bye_email(user)
+    @user = user
+    mail(to: @user.email, subject: "Hope 'll meet soon, looged Out as #{@user.email}")
 end
