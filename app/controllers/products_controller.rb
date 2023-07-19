c8db6463c9b816110691d28d2ca7ec8d72550547
ppwt <saraswatik@audaxlabs.com>
2023-07-19 15:56:53 +0530

    added signup signin before actions

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,5 +1,7 @@
 class ProductsController < ApplicationController
   before_action :set_product, only: %i[ show edit update destroy ]
+  # added login_signup 
+  before_action :authenticate_user!, except:[:index, :show]
 
   # GET /products or /products.json
   def index
